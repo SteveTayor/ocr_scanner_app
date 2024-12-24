7bd12cb676977eba0b0838e7fb0e334cf200acc2
SteveTayor <ayomidearose75@gmail.com>
2024-12-24 13:01:50 +0100

    `Added Firebase dependencies and configuration for Android app`

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
